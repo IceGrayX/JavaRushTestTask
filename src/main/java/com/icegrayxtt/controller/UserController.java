98ac7c46ab86f7f1809f13db027700912ef4ab09
Валерий <valeriinalimov@mail.ru>
2016-11-24 15:16:48 +0700

    удалил лишние импорты

--- src/main/java/com/icegrayxtt/controller/UserController.java
+++ src/main/java/com/icegrayxtt/controller/UserController.java
@@ -3,12 +3,12 @@ package com.icegrayxtt.controller;
 import com.icegrayxtt.model.User;
 import com.icegrayxtt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
 import java.util.List;
